6cfa576a1fa5ac1090db1783481b91f2eb09df63
Guilherme Toledo <totoledao@hotmail.com>
2021-09-09 16:38:22 -0300

    Started implementing date picker

--- pages/scheduler.js
+++ pages/scheduler.js
@@ -1,18 +1,47 @@
 import { 
   Center,
   Container,
-  Button
+  Box,
+  Heading,
+  Text,
+  Button,
+  IconButton
 } from "@chakra-ui/react";
-import { useEffect } from 'react';
+import {ChevronLeftIcon, ChevronRightIcon} from '@chakra-ui/icons';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { addDays, subDays } from 'date-fns';
+import axios from 'axios';
 
 import { useAuth } from '../components/Auth';
+import { dateFormat, formatDate } from '../components/DateFormat';
 import LoadingScreen from '../components/LoadingScreen';
+
+const getSchedule = ( { token, when } ) => axios({
+  method: 'get',
+  url: '/api/schedule',
+  params: {
+    when    
+  },
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
 
 export default function Scheduler() {
 
+  const [when, setWhen] = useState(() => new Date())
+
   const [auth, { logout }] = useAuth();
   const router = useRouter();
+
+  const previousDay = () => {
+    setWhen( oldValue => subDays(oldValue, 1) );
+  }
+  
+  const nextDay = () => {
+    setWhen( oldValue => addDays(oldValue, 1) );
+  }
 
   useEffect(() => {
     !auth.user && router.push('/');
@@ -23,14 +52,27 @@ export default function Scheduler() {
   }
 
   return(
-    <Center h="100vh">
-      <Container>
+    
+    <Container centerContent>
 
-      <div>Agenda</div>
-
-      <Button onClick={logout}>Sair</Button>
-
+      <Container display="flex" flexDir="row"
+        justifyContent='space-between'
+        paddingTop="1.5rem" paddingBottom="1.5rem"
+      >
+        <Heading variant="logo" > planly </Heading>  
+        <Button onClick={logout}>Sair</Button>
       </Container>
-    </Center>
+
+      <Container paddingTop={4} 
+        display="flex" flexDirection="row" 
+        justifyContent='space-between' alignItems="center"
+      >
+        <IconButton aria-label="Left" icon={<ChevronLeftIcon />} bg="transparent" onClick={previousDay}/>
+        <Text>{formatDate(when, 'PPPP')}</Text>
+        <IconButton aria-label="Right" icon={<ChevronRightIcon />} bg="transparent"  onClick={nextDay} />
+      </Container>
+
+
+    </Container>
   )
 }
